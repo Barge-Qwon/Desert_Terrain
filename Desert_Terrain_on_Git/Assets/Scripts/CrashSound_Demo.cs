23eae576f87c599b1dbe48293948e2f7b849f799
E_H <public.static.void.dog.string.args@gmail.com>
2020-09-20 15:06:48 -0700

    Initial Upload of Project

---
+++ Desert_Terrain_on_Git/Assets/Scripts/CrashSound_Demo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashSound_Demo : MonoBehaviour
+{
+
+    public AudioClip crashSoft;
+    public AudioClip crashHard;
+
+    private AudioSource source;
+
+    public float lowPitchRange = .75F;
+    public float highPitchRange = 1.25F;
+    public float influence_of_magnitude = .01F;
+    public float velocity_limit = 15F;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnCollisionEnter (Collision collide)
+    {
+        // without the condition: just play clip
+        // source.PlayOneShot(crashSoft, 1F);
+
+        source.pitch = Random.Range(lowPitchRange, highPitchRange);
+        float hitVol = collide.relativeVelocity.magnitude * influence_of_magnitude;
+        if (collide.relativeVelocity.magnitude < velocity_limit)
+            source.PlayOneShot(crashSoft, hitVol);
+        else
+            source.PlayOneShot(crashHard, hitVol);
+    }
+
+
+}
---
+++ Desert_Terrain_on_Git/Assets/Scripts/LaunchProjectile_Demo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchProjectile_Demo : MonoBehaviour
+{
+
+    public GameObject projectile;
+    public float launchVelocity = 2000f;
+
+    public AudioClip shootsound;
+   
+    public float lowVolumeRange = .5f;
+    public float highVolumeRange = 1.0f;
+
+    private AudioSource source;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            float vol = Random.Range(lowVolumeRange, highVolumeRange);
+            source.PlayOneShot(shootsound, vol);
+            //    source.PlayOneShot(shootsound, 1F);
+            GameObject launchThis = Instantiate (projectile, transform.position, transform.rotation) as GameObject;
+            launchThis.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, launchVelocity));
+        }
+    }
+}
+ 
